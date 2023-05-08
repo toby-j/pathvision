ddcb3226ce4a919bb9ba3804241fef8bb28d3744
Toby <39532961+toby-j@users.noreply.github.com>
2023-05-08 16:03:22 +0100

    kalman tracker with boolean eucledian distance check implemented. Should make tracking a class really..

--- pathvision/core/kalman.py
+++ pathvision/core/kalman.py
@@ -1,17 +1,3 @@
-# Copyright 2023 Toby Johnson. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import argparse
 import cv2 as cv
 import numpy as np
@@ -21,40 +7,31 @@ if sys.version_info[0] != 3:
     print("This module requires Python 3")
     sys.exit(1)
 
-'''transitionMatrix: 
-    Each row of the transition matrix corresponds to an element of the state vector at time t, 
-    and each column corresponds to the same element at time t+1. The diagonal elements represent the current state of 
-    each variable, and the off-diagonal elements represent how the values of these variables evolve over time.
-
-    In particular, the non-zero elements of the transition matrix indicate how the state variables are expected to 
-    change over time. For example, the first row of the matrix specifies that the position in the x direction is 
-    expected to increase by an amount equal to the velocity in the x direction multiplied by the elapsed time between 
-    updates. Similarly, the fifth and sixth rows indicate that the width and height of the bounding box are expected 
-    to remain constant, while the seventh and eighth rows indicate that the velocity in the width and height 
-    directions are expected to remain constant as well.
-
-    The identity matrix on the right-hand side of the assignment sets the initial value of the transition matrix to an 
-    identity matrix, which assumes that the state variables are uncorrelated and evolve independently over time. The 
-    subsequent assignments update the elements of the transition matrix corresponding to the position, width, and height 
-    variables to reflect the expected changes in these variables due to the elapsed time between updates.
-
-    .correct() is used to update the state of the kalman filter
-
-'''
-
 
 class KalmanBB:
+    global nstates, nmeasures, kfilt, SBBX, SBBY, SV_X, SV_Y, SBBW, SBBH, SV_W, SV_H, MBBX, MBBY, MBBW, MBBH
+    nstates = 8  # bbx, bby, vx, vy, bbw, bbh, vw, bh (8x8 matrix)
+    nmeasures = 4  # bbx, bby, bbw, bbh (4x4 matrix)
+    kfilt = cv.KalmanFilter(nstates, nmeasures, 0)
+    # state variable indices
+    SBBX = 0
+    SBBY = 1
+    SV_X = 2
+    SV_Y = 3
+    SBBW = 4
+    SBBH = 5
+    SV_W = 6
+    SV_H = 7
+    # measurement variable indices
+    MBBX = 0
+    MBBY = 1
+    MBBW = 2
+    MBBH = 3
+
     def __init__(self):
-        self.nstates = 8
-        self.measurements = 4
-        self.KF = cv.KalmanFilter(self.nstates, self.measurements, 0)
+        # For keeping track of time
         self.ticks = 0
         self.lastTicks = 0
-
-        # state variable indices
-        self.SBBX, self.SBBY, self.SV_X, self.SV_Y, self.SBBW, self.SBBH, self.SV_W, self.SV_H = range(self.nstates)
-        # measurement variable indices
-        self.MBBX, self.MBBY, self.MBBW, self.MBBH = range(self.measurements)
 
         # A: Transition State Matrix
         #   x  y vx vt  w  h vw vh
@@ -66,11 +43,7 @@ class KalmanBB:
         # [ 0  0  0  0  0  1  0 dT ] => dT at TDTH
         # [ 0  0  0  0  0  0  1  0 ]
         # [ 0  0  0  0  0  0  0  1 ]
-        self.KF.transitionMatrix = np.eye(self.nstates, dtype=np.float32)
-        self.KF.transitionMatrix[self.SBBX, self.SV_X] = 1.0
-        self.KF.transitionMatrix[self.SBBY, self.SV_Y] = 1.0
-        self.KF.transitionMatrix[self.SBBW, self.SV_W] = 1.0
-        self.KF.transitionMatrix[self.SBBH, self.SV_H] = 1.0
+        kfilt.transitionMatrix = np.eye(nstates, dtype=np.float32)
 
         # H: Measurement Matrix
         #   x  y vx vt  w  h vw vh
@@ -78,11 +51,11 @@ class KalmanBB:
         # [ 0  1  0  0  0  0  0  0 ] y
         # [ 0  0  0  0  1  0  0  0 ] w
         # [ 0  0  0  0  0  1  0  0 ] h
-        self.KF.measurementMatrix = np.zeros((self.measurements, self.nstates), dtype=np.float32)
-        self.KF.measurementMatrix[self.MBBX, self.SBBX] = 1.0
-        self.KF.measurementMatrix[self.MBBY, self.SBBY] = 1.0
-        self.KF.measurementMatrix[self.MBBW, self.SBBW] = 1.0
-        self.KF.measurementMatrix[self.MBBH, self.SBBH] = 1.0
+        kfilt.measurementMatrix = np.zeros((nmeasures, nstates), dtype=np.float32)
+        kfilt.measurementMatrix[MBBX, SBBX] = 1.0
+        kfilt.measurementMatrix[MBBY, SBBY] = 1.0
+        kfilt.measurementMatrix[MBBW, SBBW] = 1.0
+        kfilt.measurementMatrix[MBBH, SBBH] = 1.0
 
         # Q: Process Noise Covariance Matrix
         #   x    y   vx   vt    w    h    vw   vh
@@ -94,35 +67,36 @@ class KalmanBB:
         # [ 0    0    0    0    0    Ebbh 0    0   ]
         # [ 0    0    0    0    0    0    Ev_w 0   ]
         # [ 0    0    0    0    0    0    0    Ev_h]
-        self.KF.processNoiseCov = np.eye(self.nstates, dtype=np.float32) * (1e-2)
+
+        kfilt.processNoiseCov = np.eye(nstates, dtype=np.float32) * (1e-2)
         # Override velocity errors
-        self.KF.processNoiseCov[self.SV_X, self.SV_X] = 2.0
-        self.KF.processNoiseCov[self.SV_Y, self.SV_Y] = 2.0
-        self.KF.processNoiseCov[self.SV_W, self.SV_W] = 2.0
-        self.KF.processNoiseCov[self.SV_H, self.SV_H] = 2.0
+        kfilt.processNoiseCov[SV_X, SV_X] = 2.0
+        kfilt.processNoiseCov[SV_Y, SV_Y] = 2.0
+        kfilt.processNoiseCov[SV_W, SV_W] = 2.0
+        kfilt.processNoiseCov[SV_H, SV_H] = 2.0
 
         # R: Measurement Noise Covariance Matrix
         # Higher value increases uncertainty to give more weight to prediction
         # Lower value decreases uncertainty to give more weight to measurement
-        self.KF.measurementNoiseCov = np.eye(self.measurements, dtype=np.float32) * 0.1
+        kfilt.measurementNoiseCov = np.eye(nmeasures, dtype=np.float32) * (0.08)
 
     def init(self, meas):
-        state = np.zeros(self.KF.statePost.shape, np.float32)
-        state[self.SBBX] = meas[0]
-        state[self.SBBY] = meas[1]
-        state[self.SV_X] = 0
-        state[self.SV_Y] = 0
-        state[self.SBBW] = meas[2]
-        state[self.SBBH] = meas[3]
-        state[self.SV_W] = 0
-        state[self.SV_H] = 0
-        self.KF.statePost = state
-        self.KF.statePre = state
+        state = np.zeros(kfilt.statePost.shape, np.float32)
+        state[SBBX] = meas[0]
+        state[SBBY] = meas[1]
+        state[SV_X] = 0
+        state[SV_Y] = 0
+        state[SBBW] = meas[2]
+        state[SBBH] = meas[3]
+        state[SV_W] = 0
+        state[SV_H] = 0
+        kfilt.statePost = state
+        kfilt.statePre = state
         self.lastTicks = self.ticks
         self.ticks = cv.getTickCount()
         return meas
 
-    def make_prediction(self, dT=-1):  # get predicted state
+    def predict(self, dT=-1):  # get predicted state
         # Updating the time
         self.lastTicks = self.ticks
         self.ticks = cv.getTickCount()
@@ -132,44 +106,67 @@ class KalmanBB:
             dT = 1.0 * (self.ticks - self.lastTicks) / cv.getTickFrequency()
 
         # Update the transition Matrix A with dT for this stamp
-        self.KF.transitionMatrix[self.SBBX, self.SV_X] = dT
-        self.KF.transitionMatrix[self.SBBY, self.SV_Y] = dT
-        self.KF.transitionMatrix[self.SBBW, self.SV_W] = dT
-        self.KF.transitionMatrix[self.SBBH, self.SV_H] = dT
+        kfilt.transitionMatrix[SBBX, SV_X] = dT
+        kfilt.transitionMatrix[SBBY, SV_Y] = dT
+        kfilt.transitionMatrix[SBBW, SV_W] = dT
+        kfilt.transitionMatrix[SBBH, SV_H] = dT
 
-        # Step the prediction, we now need to normalise our new coordinates
-        self.KF.predict()
+        kfilt.predict()
 
         # Keeping the values greater than or equal to 0
-        self.KF.statePre[self.SBBX] = max(0.0, self.KF.statePre[self.SBBX])
-        self.KF.statePre[self.SBBY] = max(0.0, self.KF.statePre[self.SBBY])
-        self.KF.statePre[self.SBBW] = max(0.0, self.KF.statePre[self.SBBW])
-        self.KF.statePre[self.SBBH] = max(0.0, self.KF.statePre[self.SBBH])
+        kfilt.statePre[SBBX] = max(0.0, kfilt.statePre[SBBX])
+        kfilt.statePre[SBBY] = max(0.0, kfilt.statePre[SBBY])
+        kfilt.statePre[SBBW] = max(0.0, kfilt.statePre[SBBW])
+        kfilt.statePre[SBBH] = max(0.0, kfilt.statePre[SBBH])
 
         # Returning the predicted values for bbx, bby, bbw, and bbh
         return np.float32(
-            [self.KF.statePre[self.SBBX], self.KF.statePre[self.SBBY], self.KF.statePre[self.SBBW],
-             self.KF.statePre[self.SBBH]]).squeeze()
+            [kfilt.statePre[SBBX], kfilt.statePre[SBBY], kfilt.statePre[SBBW], kfilt.statePre[SBBH]]).squeeze()
 
-    def correct_current_location(self):  # state correction using measurement matrix with BB
+    def correct(self, bbx, bby, bbw, bbh, restart=False):  # state correction using measurement matrix with BB
+
+        if restart:
+            self.ticks = cv.getTickCount()
+            cv.setIdentity(kfilt.errorCovPre, 1.0)
+
+            # Updating statePost with bbx, bby, bbw, and bbh
+            kfilt.statePost[SBBX] = bbx
+            kfilt.statePost[SBBX] = bby
+            kfilt.statePost[SV_X] = 0
+            kfilt.statePost[SV_Y] = 0
+            kfilt.statePost[SBBW] = bbw
+            kfilt.statePost[SBBH] = bbh
+            kfilt.statePost[SV_W] = 0
+            kfilt.statePost[SV_H] = 0
+
+        else:
+            # Running correct with bbx, bby, bbw, and bbh
+            kfilt.correct(np.float32([bbx, bby, bbw, bbh]).squeeze())
 
         # Keeping the values greater than or equal to 0
-        self.KF.statePost[self.SBBX] = max(0.0, self.KF.statePost[self.SBBX])
-        self.KF.statePost[self.SBBY] = max(0.0, self.KF.statePost[self.SBBY])
-        self.KF.statePost[self.SBBW] = max(0.0, self.KF.statePost[self.SBBW])
-        self.KF.statePost[self.SBBH] = max(0.0, self.KF.statePost[self.SBBH])
+        kfilt.statePost[SBBX] = max(0.0, kfilt.statePost[SBBX])
+        kfilt.statePost[SBBY] = max(0.0, kfilt.statePost[SBBY])
+        kfilt.statePost[SBBW] = max(0.0, kfilt.statePost[SBBW])
+        kfilt.statePost[SBBH] = max(0.0, kfilt.statePost[SBBH])
 
         # Return a floating point array with the corrected values for bbx, bby, bbw, bbh
         return np.float32(
-            [self.KF.statePost[self.SBBX], self.KF.statePost[self.SBBY], self.KF.statePost[self.SBBW],
-             self.KF.statePost[self.SBBH]]).squeeze()
-
-    def iterate(self, bb):
-        self.KF.correct(np.float32(bb).squeeze())
+            [kfilt.statePost[SBBX], kfilt.statePost[SBBY], kfilt.statePost[SBBW], kfilt.statePost[SBBH]]).squeeze()
 
     def getPostState(self):  # get the state after correction
         # Return a floating point array with the correct values for bbx, bby, bbw, bbh
         # Use if you want to get the state after correction without actually running correction again
         return np.float32(
-            [self.KF.statePost[self.SBBX], self.KF.statePost[self.SBBY], self.KF.statePost[self.SBBW],
-             self.KF.statePost[self.SBBH]]).squeeze()
+            [kfilt.statePost[SBBX], kfilt.statePost[SBBY], kfilt.statePost[SBBW], kfilt.statePost[SBBH]]).squeeze()
+
+    def track(self, bbx, bby, bbw, bbh, dT=-1, onlyPred=False):
+        if onlyPred:
+            pred = self.predict(dT)
+            cpred = self.correct(pred[0], pred[1], pred[2], pred[3], False)
+        else:
+            self.predict(dT)
+            cpred = self.correct(bbx, bby, bbw, bbh, False)
+        return cpred
+
+
+
--- pathvision/core/tracking.py
+++ pathvision/core/tracking.py
@@ -1,9 +1,36 @@
 import math
-
+from pathvision.core import kalman
+import numpy as np
 
 def _euclidean_distance(x1, y1, x2, y2):
     return math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
 
+def calculate_error(kalman_prediction, model_bbox) -> bool:
+    """
+    Calculates the error between the predicted bounding box from the Kalman filter
+    and the actual bounding box from the object detection model.
+
+    Args:
+        kalman_prediction (tuple): Predicted bounding box from the Kalman filter.
+            The bounding box is represented as a tuple (x, y, w, h).
+        model_bbox (tuple): Actual bounding box from the object detection model.
+            The bounding box is represented as a tuple (x, y, w, h).
+
+    Returns:
+        bool: If the difference in the boxes meets our threshold
+    """
+
+    # Calculate the center coordinates of the predicted and actual bounding boxes
+    kalman_center = (kalman_prediction[0] + kalman_prediction[2] / 2, kalman_prediction[1] + kalman_prediction[3] / 2)
+    model_center = (model_bbox[0] + model_bbox[2] / 2, model_bbox[1] + model_bbox[3] / 2)
+
+    # Calculate the Euclidean distance between the center coordinates
+    inaccuracy = math.sqrt((kalman_center[0] - model_center[0])**2 + (kalman_center[1] - model_center[1])**2)
+    print("inaccuracy: {}".format(inaccuracy))
+    if inaccuracy > 100:
+        return True
+    else:
+        return False
 
 def _rank_boxes(bboxes, target_bbox):
     target_x, target_y = (target_bbox[0] + target_bbox[2]) / 2, (target_bbox[1] + target_bbox[3]) / 2
@@ -17,7 +44,18 @@ def _rank_boxes(bboxes, target_bbox):
     return ranked_bboxes
 
 
-def iterate_kalman_tracker(class_idxs, bb_boxes, kalman_tracker, tracking):
+def _create_tracking_entry(class_idx, box, kalman_tracker):
+    if len(kalman_tracker[class_idx].keys()) == 0:
+        obj_dict = kalman_tracker[class_idx].setdefault("0", {})
+    else:
+        obj_dict = kalman_tracker[class_idx].setdefault(str(len(kalman_tracker[class_idx].keys()) + 1), {})
+    obj_dict['boxes'] = [[box]]
+    obj_dict.setdefault('kalman', kalman.KalmanBB()).init(np.float32(box.detach()))
+
+
+def iterate_kalman_tracker(class_idxs, bb_boxes, kalman_tracker) -> dict:
+    fps = 300.
+    dT = (1 / fps)
     # For each unique class that our model has seen
     for class_idx in list(set(class_idxs)):
         # For this class, collect the indexes of where this class is in class_idxs
@@ -35,26 +73,48 @@ def iterate_kalman_tracker(class_idxs, bb_boxes, kalman_tracker, tracking):
             # For how many objects of this class we're already tracking, we collect the end box of each. We then
             # have the latest position of these tracked objects. We use these to decide where to place our new
             # boxes.
+
             for key in kalman_tracker[class_idx].keys():
                 # Get the last box of each object
-                object_bbs.append(kalman_tracker[class_idx][key][0][:1][0])
+                object_bbs.append(kalman_tracker[class_idx][key]['boxes'][0][:1][0])
 
             # For each existing end box, see which of the new boxes is most appropriate to append
             for i, box in enumerate(object_bbs):
                 ranked_bboxes = _rank_boxes(boxes_to_place, box)
                 # Append new location to object
-                kalman_tracker[class_idx][str(i)].append(ranked_bboxes[0][0])
+                kalman = kalman_tracker[class_idx][str(i)]["kalman"]
+                # We initialise with previously known boxes
+                # kalman.iterate(np.float32(box))
+
+                # Kalman test
+                x, y, w, h = ranked_bboxes[0][0]
+                tracked = kalman.track(x, y, w, h, dT)
+                print("tracked position: {}".format(tracked))
+                pred = kalman.predict()
+                print("predicted position: {}".format(pred))
+                print("Models bb: {}".format(ranked_bboxes[0][0]))
+
+                error = calculate_error(pred, ranked_bboxes[0][0])
+
+                print(error)
+
+                kalman_tracker[class_idx][str(i)]['boxes'].append(ranked_bboxes[0][0])
+
                 boxes_to_place.remove(ranked_bboxes[0][0])
+
                 # If we have less new boxes than total tracked objects, we'll run out of new boxes.
                 if len(boxes_to_place) == 0:
                     break
+
             # For remaining boxes, initialise a new object inside the class to begin tracking
             for box in boxes_to_place:
-                kalman_tracker.setdefault(class_idx, {})[len(kalman_tracker[class_idx].keys()) + 1] = [[box]]
+                _create_tracking_entry(class_idx, box, kalman_tracker)
 
         else:
             # We're not yet tracking this class.
             # For our box we have for this class, initialise a new tracking entry.
-            for i, bb_box in enumerate(class_bb_boxes):
-                kalman_tracker.setdefault(class_idx, {})[str(i)] = [[bb_box]]
-        return kalman_tracker
+            kalman_tracker.setdefault(class_idx, {})
+            for box in class_bb_boxes:
+                _create_tracking_entry(class_idx, box, kalman_tracker)
+
+    return kalman_tracker
